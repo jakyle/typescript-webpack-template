bb882f4d0643a0f93517fe72984f603d05261731
Jakyle <jjackson360@gmail.com>
2017-10-24 13:22:16 -0500

    travis test2

--- src/helloWorld.test.ts
+++ src/helloWorld.test.ts
@@ -6,6 +6,6 @@ describe('Hello function', () => {
 
   it('should return hello world', () => {
     const result = hello();
-    expect(result).to.equal('Hello gorld!');
+    expect(result).to.equal('Hello world!');
   });
 });
